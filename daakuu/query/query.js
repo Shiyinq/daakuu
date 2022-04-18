f78146a84ba8e7036baa13cad4a64f2f23246053
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2022-04-18 11:23:12 +0700

    update query & showing detail anime when user click number on paging

--- daakuu/query/query.js
+++ daakuu/query/query.js
@@ -1,17 +1,43 @@
 module.exports = `
-query ($page: Int, $perPage: Int, $type: MediaType, $seasonYear: Int, $season: MediaSeason, $format_in: [MediaFormat], $sort: [MediaSort]) {
+query ($page: Int, $perPage: Int, $id: Int, $type: MediaType, $seasonYear: Int, $season: MediaSeason, $format_in: [MediaFormat], $sort: [MediaSort]) {
 	Page(page: $page, perPage: $perPage) {
 		pageInfo {
 			perPage
 			currentPage
 			hasNextPage
 		}
-		media(type: $type, seasonYear: $seasonYear, season: $season, format_in: $format_in, sort: $sort) {
+		media(id: $id, type: $type, seasonYear: $seasonYear, season: $season, format_in: $format_in, sort: $sort) {
 			id
 			title {
-				romaji
-				userPreferred
+			  romaji
+			  userPreferred
 			}
+			coverImage {
+			  extraLarge
+			  large
+			  medium
+			  color
+			}
+			description
+			format
+			episodes
+			duration
+			status
+			startDate {
+			  year
+			  month
+			  day
+			}
+			season
+			meanScore
+			studios {
+			  nodes {
+				name
+			  }
+			}
+			source
+			genres
+			siteUrl
 		}
 	}
 }
