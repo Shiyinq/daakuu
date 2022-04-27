fd6de1038a32e0aa92bb140a5f1284efbf8a3c75
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2022-04-27 20:54:11 +0700

    add new command about

--- bot.js
+++ bot.js
@@ -66,6 +66,12 @@ bot.command(['/top_50_manga', '/t50m'], (ctx) => {
 	top50Manga(ctx, 0)
 })
 
+bot.command(['/about'], (ctx) => {
+	ctx.replyWithMarkdown('DaAkuu (Daftar Anime Kuu) is unofficial anilis.co bot.\n\nYou can visit our [repository](https://github.com/Shiyinq/daakuu) to contribute or submit issue.',{ 
+		disable_web_page_preview: true 
+	})
+})
+
 bot.on('callback_query', (ctx) => {
 	let dataQuery = ctx.callbackQuery.data
 	let [callFunction, page] = dataQuery.split('-')
